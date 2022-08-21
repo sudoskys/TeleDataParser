aea9af86b773e4ff029fffda91ee70e6c226ddf5
sudoskys <coldlando@hotmail.com>
2022-08-21 13:42:38 +0800

    feat:add min limit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ inputDir = mew.get('path', 'input')
 outDir = mew.get('path', 'output')
 
 if __name__ == "__main__":
-    Run = TeleParser(inputDir, outDir, min_limit=5, max_limit=512 * 2)
+    Run = TeleParser(inputDir, outDir, min_limit=5, max_limit=512)
     total_num, skip_num, delete_num, all_num = Run.get_reply(lable, target_id, showDate=False)
 
 
